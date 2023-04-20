e6f93116bf60cbc0a2309cf3d03a53aaa6297912
ShelfVTS <53570751+ShelfVTS@users.noreply.github.com>
2023-04-20 07:58:04 -0400

    Add files via upload

---
+++ mousey_mover.py
@@ -0,0 +1,17 @@
+import pyautogui
+import random
+
+list1 = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900]
+list2 = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+i = 0
+
+while(i < 5):
+    x = random.choice(list1)
+    y = random.choice(list2)
+
+    pyautogui.moveTo(x, y, duration = .2)
+else: 
+    i += 1
+
+    
